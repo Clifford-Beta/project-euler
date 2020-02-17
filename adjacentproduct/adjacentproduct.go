79fd26aa110ff131d9930ba30dea288943df1041
Clifford <betaclifford@gmail.com>
2020-02-17 21:47:17 +0300

    Rename workflow `fmt` Signed-off-by: Clifford <betaclifford@gmail.com>

---
+++ adjacentproduct/adjacentproduct.go
@@ -0,0 +1,34 @@
+package adjacentproduct
+
+import (
+	"strconv"
+)
+
+func adjacentNDigits(number string, n int) ([]int, int ){
+	maxProduct := 0
+	digits := []int{}
+	noOfDigits := len(number)
+	for i := 0;i<noOfDigits ;i++  {
+		j := i+n
+		if j >= noOfDigits {
+			break
+		}
+		dgts, product := calculateProduct(number[i:j])
+		if product > maxProduct{
+			digits, maxProduct = dgts, product
+		}
+		//calculate product
+	}
+	return digits, maxProduct
+}
+
+func calculateProduct(num string) ([]int, int)  {
+	numbers := []int{}
+	product := 1
+	for _, rune := range num {
+		val, _ := strconv.Atoi(string(rune))
+		product *= val
+		numbers = append(numbers, val)
+	}
+	return numbers, product
+}
